eb3371dfa7556fa1225840724a846c68c308c871
aehrath <aehrath@hipergames.com>
2025-05-28 18:50:32 -0700

    Magic

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("My son better not be lazy!")
+from dataclasses import dataclass
+
+@dataclass
+class Node:
+    pass
+
+@dataclass
+class Node:
+    name: str = ""
+    left: Node = Node()
+    right: Node = Node()
+
+test_node: Node = Node()
+left_node: Node = Node()
+right_node: Node = Node()
+
+test_node.right = right_node
+test_node.left = left_node
+test_node.name = "test_node"
+right_node.name = "right_node"
+left_node.name = "left_node"
+
+print(test_node.name)
+print(left_node.name)
+print(right_node.name)
